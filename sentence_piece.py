6f4c0f02e03308d3ef01f7baed0e7406426b9f96
MrBinit <sapkotabinit2002@gmail.com>
2025-03-11 21:12:20 +0545

    converted sentence piece to tiktoken format

--- sentence_piece.py
+++ sentence_piece.py
@@ -3,7 +3,7 @@ import os
 import logging
 from tiktoken.load import load_tiktoken_bpe
 
-
+import json
 
 #setup logging
 
@@ -22,9 +22,31 @@ def train_nepali_tokenizer(input_file: str, model_prefix: str, vocab_size: int =
 
 # convert sentencepiece  model to tiktoken compatible format
 
+def convert_sentencepiece_to_tiktoken(model_prefix: str, output_file:str):
+    """
+    Converts a SentencePiece model to a mergeable ranks file expected by tiktoken.
+    """
+    sp = spm.SentencePieceProcessor()
+    sp.load(f"{model_prefix}.model")
+    mergeable_ranks = {}
+    for i in range(sp.get_piece_size()):
+        piece = sp.id_to_piece(i)
+        mergeable_ranks[piece] = i
+
+    with open(output_file, "w", encoding = "utf-8") as f:
+        json.dump(mergeable_ranks, f, ensure_ascii=False, indent=2)
+    logger.info(f"Converted SentencePiece model to tiktoken format: {output_file}")
+
 corpus_file = '/home/binit/fine_tune_LLama/nepali_text.txt'
 model_prefix = 'nepali_tokenizer'
+mergeable_ranks_file = "nepali_mergeable_ranks.json"
 if not os.path.exists(f"{model_prefix}.model"):
     train_nepali_tokenizer(corpus_file, model_prefix)
 else:
     print("already exists")
+
+if not os.path.exists(mergeable_ranks_file):
+    logger.info("Converting sentencePiece model to tiktoken format.....")
+    convert_sentencepiece_to_tiktoken(model_prefix, mergeable_ranks_file)
+else:
+    logger.info("Mergeable ranks file already exists....")
