5b48268cf4c82386f277b390696735b4f8963b2e
MrBinit <sapkotabinit2002@gmail.com>
2025-03-15 21:27:17 +0545

    made small changes in conversion of text csv to text

--- test_tokenizer.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoTokenizer
-
-model_name = "/home/binit/fine_tune_LLama/Llama-3.2-3B"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-text = "नेपालको राजधानी काठमाडौँ हो।"
-tokens = tokenizer.tokenize(text)
-
-print(tokens)
